0bcab649632dba29fdae5cf3bbe6094600386ad7
Yamiko <yamikom@gmail.com>
2015-01-27 13:07:12 +0200

    Updated DNA PCR extract

--- data_extracts/dna_pcr_test_extract.sql
+++ data_extracts/dna_pcr_test_extract.sql
@@ -3,7 +3,9 @@ SELECT
 'Participant Number',
 'Testing sample date',
 'HIV test result'                          ,
+'Results received'                          ,
 'Date results received'                          ,
+'Results given',
 'Date results given',
 'Double data entered',
 'Notes'                          
@@ -13,12 +15,14 @@ location_for_obs(encounter_id, 7759) AS Facility_code,
 participant_id(encounter_id), 
 text_for_obs(encounter_id, 8047) AS Testing_sample_date,
 text_for_obs(encounter_id, 2169) AS HIV_test_result,
+text_for_obs(encounter_id, 8432) AS Result_received,
 text_for_obs(encounter_id, 8053) AS Result_received_date,
+text_for_obs(encounter_id, 8431) AS Result_given,
 text_for_obs(encounter_id, 8059) AS Result_given_date,
 text_for_obs(encounter_id, 8411) AS Double_data_entered,
 text_for_obs(encounter_id, 2688) AS Notes
  FROM encounter 
-WHERE encounter_type = 117 AND (DATEDIFF(encounter_datetime, dob(patient_id))/365) < 5 AND voided = 0 
+WHERE encounter_type = 117 AND voided = 0 
 INTO OUTFILE '/tmp/infant_dna_pcr_test.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
