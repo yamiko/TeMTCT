120d9ecf9260660c3ac5c29ce2af12c5bd911b7c
Yamiko <yamikom@gmail.com>
2016-02-11 11:15:11 +0200

    Updated unlock script

--- db/unlock_db.sql
+++ db/unlock_db.sql
@@ -1,1 +1,1 @@
-UPDATE databasechangeloglock SET locked=false, lockgranted=null, lockedby=null WHERE id=1;
+UPDATE liquibasechangeloglock SET locked=false, lockgranted=null, lockedby=null WHERE id=1;
